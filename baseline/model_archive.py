1a0780d280c43bb3e82de00989e1ecf925fa7b43
Piorosen <aoikazto@naver.com>
2023-03-30 17:09:19 -0700

    Update model_archive.py

--- baseline/model_archive.py
+++ baseline/model_archive.py
@@ -1,6 +1,9 @@
 import torch
 import torchvision
 
+# set the model to inference mode 
+
+
 
 def resnet18():
     model = torchvision.models.resnet18(weights="IMAGENET1K_V1")
@@ -8,21 +11,47 @@ def resnet18():
     model.eval()
     return model
 
-
 def resnet50():
     model = torchvision.models.resnet50(weights="IMAGENET1K_V1")
     model.to("cpu")
     model.eval()
     return model
 
+def mobilenetv2():
+    mbn2 = torch.hub.load('pytorch/vision:v0.10.0', 'mobilenet_v2', pretrained=True)
+    mbn2.eval() 
+    return mbn2
+    
+def vgg16():
+    vgg = torch.hub.load('pytorch/vision:v0.10.0', 'vgg16', pretrained=True)
+    vgg.eval() 
+    return vgg
+    
+def googlenet():
+    goog = torch.hub.load('pytorch/vision:v0.10.0', 'googlenet', pretrained=True)
+    goog.eval() 
+    return goog
+    
+def alexnet():
+    alex = torch.hub.load('pytorch/vision:v0.10.0', 'alexnet', pretrained=True)
+    alex.eval() 
+    return alex
 
 MODEL_ARCHIVE = {
-    "resnet18": {
-        "model": resnet18,
+    "alexnet": {
+        "model": alexnet,
+        "input": (torch.randn(1, 3, 227, 227),),
+    },
+    "vgg16": {
+        "model": vgg16,
         "input": (torch.randn(1, 3, 224, 224),),
     },
-    "resnet50": {
-        "model": resnet50,
+    "googlenet": {
+        "model": googlenet,
+        "input": (torch.randn(1, 3, 224, 224),),
+    },
+    "mobilenetv2": {
+        "model": mobilenetv2,
         "input": (torch.randn(1, 3, 224, 224),),
     },
 }
